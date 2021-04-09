7ee0c34c670edf0b779916d338a6bbfad8fff89c
Will K <31964565+Knapw13@users.noreply.github.com>
2021-04-08 23:01:00 -0400

    Making anon function for Timeout

--- index.js
+++ index.js
@@ -6,26 +6,12 @@ function rollThemBones() {
   var audio = new Audio("images/diceRoll.wav");
   audio.play();
   
-  window.setTimeout(updateDiceImages, 1300);
-
-
-  if(playerOne>playerTwo) {
-    //playerOneWins change
-  }
-  else {
-    //playerTwo win change
-  }
-
-  sleep(2000);
-
-  //change back to original state.
+  window.setTimeout(function () {
+    var playerOne = Math.floor(Math.random() * 6) + 1;
+    var playerTwo = Math.floor(Math.random() * 6) + 1;
+    var diceOne = "images/dice" + playerOne + ".png";
+    var diceTwo = "images/dice" + playerTwo + ".png";
+    document.querySelector(".img1").src = diceOne;
+    document.querySelector(".img2").src = diceTwo;
+    }, 1300);
 }
-
-function updateDiceImages() {
-  var playerOne = Math.floor(Math.random() * 6) + 1;
-  var playerTwo = Math.floor(Math.random() * 6) + 1;
-  var diceOne = "images/dice" + playerOne + ".png";
-  var diceTwo = "images/dice" + playerTwo + ".png";
-  document.querySelector(".img1").src = diceOne;
-  document.querySelector(".img2").src = diceTwo;
- }
