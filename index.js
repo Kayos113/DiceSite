1c19b44fba2452b5f50aeca6598a1b0c8979e0ca
Will K <31964565+Knapw13@users.noreply.github.com>
2021-04-08 22:09:17 -0400

    add Audio

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function rollThemBones() {
   var playerOne = Math.floor(Math.random() * 6) + 1;
   var playerTwo = Math.floor(Math.random() * 6) + 1;
 
+  // var redDice = "images/redDice.png";
+  // document.querySelector(".img1").src = redDice;
+  // document.querySelector(".img2").src = redDice;
+
+  var audio = new Audio("images/diceRoll.wav");
+  sleep(2000);
+
   var diceOne = "images/dice" + playerOne + ".png";
   var diceTwo = "images/dice" + playerTwo + ".png";
   document.querySelector(".img1").src = diceOne;
