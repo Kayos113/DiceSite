b248f3a3d44774872d6eeb9504561fe2af18daae
Will <willyknapp@gmail.com>
2021-04-09 00:47:42 -0400

    Adding dice rotation

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function rollThemBones() {
-  // var redDice = "images/redDice.png";
-  // document.querySelector(".img1").src = redDice;
-  // document.querySelector(".img2").src = redDice;
 
   var audio = new Audio("images/diceRoll.wav");
   audio.play();
-  
-  window.setTimeout(function () {
-    var playerOne = Math.floor(Math.random() * 6) + 1;
-    var playerTwo = Math.floor(Math.random() * 6) + 1;
-    var diceOne = "images/dice" + playerOne + ".png";
-    var diceTwo = "images/dice" + playerTwo + ".png";
-    document.querySelector(".img1").src = diceOne;
-    document.querySelector(".img2").src = diceTwo;
-    }, 1300);
+
+  var rand;
+  var redDice = "images/redDice.png";
+  document.querySelector(".img1").src = redDice;
+  document.querySelector(".img2").src = redDice;
+  for(var i=0; i<1300; i+=rand)
+  {
+    rand = Math.floor(Math.random()*75)+1
+    window.setTimeout(function() {
+      document.querySelector(".img1").style.transform = 'rotate('+rand+'deg)';
+      document.querySelector(".img2").style.transform = 'rotate('+(rand*-1)+'deg)';
+    }, rand);
+  }
+  document.querySelector(".img1").style.transform = 'rotate('+0+'deg)';
+  document.querySelector(".img2").style.transform = 'rotate('+0+'deg)';
+
+  var playerOne = Math.floor(Math.random() * 6) + 1;
+  var playerTwo = Math.floor(Math.random() * 6) + 1;
+  var diceOne = "images/dice" + playerOne + ".png";
+  var diceTwo = "images/dice" + playerTwo + ".png";
+  document.querySelector(".img1").src = diceOne;
+  document.querySelector(".img2").src = diceTwo;
 }
